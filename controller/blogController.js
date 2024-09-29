7b07b11c4543655b84f8a0aba1ebd00c3ae80d12
Meet763 <meetkathiriya596@gmail.com>
2024-09-29 16:52:56 +0530

    crud operation functionality

--- controller/blogController.js
+++ controller/blogController.js
@@ -23,4 +23,42 @@ const getBlogs = async (req, res) => {
     }
 }
 
-module.exports = {createBlog, getBlogs};
+const updateBlog = async (req, res) => {
+    try{
+        const blogId = req.params.id;
+        const updatedBlog = req.body;
+        const data = await Blog.findOneAndUpdate({user: req.user.id, _id: blogId}, updatedBlog, {
+            new: true, 
+            runValidators: true
+        })
+
+        if (!data){
+            return res.status(404).json({ error: 'blog not found' }) ;
+        }
+
+        console. log ('data updated') ; 
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'internal server error'});
+    }
+}
+
+const deleteBlog = async (req, res) => {
+    try{
+        const blogId = req.params.id;
+        const data = await Blog.findOneAndDelete({user: req.user.id, _id: blogId})
+
+        if (!data){
+            return res.status(404).json({ error: 'blog not found' }) ;
+        }
+
+        console. log ('deleted') ; 
+        res.status(200).json(data);
+    }catch(err){
+        console.log(err);
+        res.status(500).json({error: 'internal server error'});
+    }
+}
+
+module.exports = {createBlog, getBlogs, updateBlog, deleteBlog};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router();
-const {createBlog, getBlogs} = require('../controller/blogController')
+const {createBlog, getBlogs, updateBlog, deleteBlog} = require('../controller/blogController')
 const {jwtAuthMiddleware} = require('../middleware/auth')
 
 router.post('/create', jwtAuthMiddleware, createBlog);
 
 router.get('/getblogs', jwtAuthMiddleware, getBlogs);
+
+router.put('/update/:id', jwtAuthMiddleware, updateBlog);
+
+router.delete('/delete/:id', jwtAuthMiddleware, deleteBlog);
 
 
 module.exports = router;
